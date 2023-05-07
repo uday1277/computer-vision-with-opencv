ed534c3942ab404b0cc9d042150ee87a47e88213
uday1277 <113880624+uday1277@users.noreply.github.com>
2023-05-07 19:47:18 +0530

    Create 8. Rotation of img(180).py

---
+++ 8. Rotation of img(180).py
@@ -0,0 +1,7 @@
+import cv2
+path = r"C:\Users\koppo\OneDrive\Pictures\Naruto WP\wallpaperflare.com_wallpaper.jpg"
+src = cv2.imread(path)
+window_name = "Ima"
+image = cv2.rotate(src, cv2.ROTATE_180)
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
